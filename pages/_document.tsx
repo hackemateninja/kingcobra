318f9618769e6c1e5ee67da850989fb2873f5e04
Pablo.Colaiacovo <Pablo.Colaiacovo@perficient.com>
2021-02-01 13:53:47 +0000

    Merged PR 26: Added GTM, UTS Cookie and Carcom Theme
    Added GTM, UTS Cookie and Carcom Theme

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,59 +1,88 @@
-import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
-import { ServerStyleSheet } from 'styled-components';
+import Document, { Html, Head, Main, NextScript, DocumentContext } from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
-	static async getInitialProps( ctx: DocumentContext ) {
-		const sheet = new ServerStyleSheet();
-		const originalRenderPage = ctx.renderPage;
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
 
-		try {
-			ctx.renderPage = () =>
-				originalRenderPage({
-					enhanceApp: ( App ) => ( props ) =>
-						sheet.collectStyles( <App {...props} /> ),
-				});
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+        });
 
-			const initialProps = await Document.getInitialProps( ctx );
-			return {
-				...initialProps,
-				styles: (
-					<>
-						{initialProps.styles}
-						{sheet.getStyleElement()}
-					</>
-				),
-			};
-		} finally {
-			sheet.seal();
-		}
-	}
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
 
-	render() {
+  render() {
+    // const loadVWO = `
+    //   window._vwo_code = window._vwo_code || (function(){
+    //   var account_id=428746,
+    //   settings_tolerance=2000,
+    //   library_tolerance=2500,
+    //   use_existing_jquery=false,
+    //   is_spa=1,
+    //   hide_element='body',
 
-		// const loadVWO = `
-		//   window._vwo_code = window._vwo_code || (function(){
-		//   var account_id=428746,
-		//   settings_tolerance=2000,
-		//   library_tolerance=2500,
-		//   use_existing_jquery=false,
-		//   is_spa=1,
-		//   hide_element='body',
+    //   /* DO NOT EDIT BELOW THIS LINE */
+    //   f=false,d=document,code={use_existing_jquery:function(){return use_existing_jquery;},library_tolerance:function(){return library_tolerance;},finish:function(){if(!f){f=true;var a=d.getElementById('_vis_opt_path_hides');if(a)a.parentNode.removeChild(a);}},finished:function(){return f;},load:function(a){var b=d.createElement('script');b.src=a;b.type='text/javascript';b.innerText;b.onerror=function(){_vwo_code.finish();};d.getElementsByTagName('head')[0].appendChild(b);},init:function(){
+    //   window.settings_timer=setTimeout('_vwo_code.finish()',settings_tolerance);var a=d.createElement('style'),b=hide_element?hide_element+'{opacity:0 !important;filter:alpha(opacity=0) !important;background:none !important;}':'',h=d.getElementsByTagName('head')[0];a.setAttribute('id','_vis_opt_path_hides');a.setAttribute('type','text/css');if(a.styleSheet)a.styleSheet.cssText=b;else a.appendChild(d.createTextNode(b));h.appendChild(a);this.load('https://dev.visualwebsiteoptimizer.com/j.php?a='+account_id+'&u='+encodeURIComponent(d.URL)+'&f='+(+is_spa)+'&r='+Math.random());return settings_timer; }};window._vwo_settings_timer = code.init(); return code; }());
+    // `;
 
-		//   /* DO NOT EDIT BELOW THIS LINE */
-		//   f=false,d=document,code={use_existing_jquery:function(){return use_existing_jquery;},library_tolerance:function(){return library_tolerance;},finish:function(){if(!f){f=true;var a=d.getElementById('_vis_opt_path_hides');if(a)a.parentNode.removeChild(a);}},finished:function(){return f;},load:function(a){var b=d.createElement('script');b.src=a;b.type='text/javascript';b.innerText;b.onerror=function(){_vwo_code.finish();};d.getElementsByTagName('head')[0].appendChild(b);},init:function(){
-		//   window.settings_timer=setTimeout('_vwo_code.finish()',settings_tolerance);var a=d.createElement('style'),b=hide_element?hide_element+'{opacity:0 !important;filter:alpha(opacity=0) !important;background:none !important;}':'',h=d.getElementsByTagName('head')[0];a.setAttribute('id','_vis_opt_path_hides');a.setAttribute('type','text/css');if(a.styleSheet)a.styleSheet.cssText=b;else a.appendChild(d.createTextNode(b));h.appendChild(a);this.load('https://dev.visualwebsiteoptimizer.com/j.php?a='+account_id+'&u='+encodeURIComponent(d.URL)+'&f='+(+is_spa)+'&r='+Math.random());return settings_timer; }};window._vwo_settings_timer = code.init(); return code; }());
-		// `;
-
-		return (
-			<Html lang="en">
-				<Head>
-					{/* <script dangerouslySetInnerHTML={{__html: loadVWO}} /> */}
-				</Head>
-				<body>
-					<Main />
-					<NextScript />
-				</body>
-			</Html>
-		);
-	}
+    return (
+      <Html lang="en">
+        <Head>
+          {/* <script dangerouslySetInnerHTML={{__html: loadVWO}} /> */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                  })(window,document,'script','dataLayer','GTM-KCJWZBS');
+                `,
+            }}
+          ></script>
+        </Head>
+        <body>
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-KCJWZBS"
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            ></iframe>
+          </noscript>
+          <Main />
+          <NextScript />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                var trackingConfig = {
+                  applicationId: 30, 
+                  trackingApi: "/api/collect/v1", 
+                  utsUrl: "https://uts.aws.abtl.io"
+                };
+              `,
+            }}
+          ></script>
+          <script src="https://static.car.com/cars/assets/js/uts-service-uri-storage.js"></script>
+        </body>
+      </Html>
+    );
+  }
 }
