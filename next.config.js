0e5c54ac051d27f4262793d82b1d7440e50481bf
Felix.Mushinsky <Felix.Mushinsky@perficient.com>
2021-02-04 20:47:03 +0000

    Merged PR 40: asset caching

--- next.config.js
+++ next.config.js
@@ -1,6 +1,37 @@
 module.exports = {
     target: 'serverless',
     images: {
-      deviceSizes: [320, 768, 1024],
+        deviceSizes: [320, 768, 1024],
     },
+    async headers() {
+        return [
+            {
+                source: '/:path(.+\\.jpg)*',
+                headers: [
+                    {
+                        key: 'cache-control',
+                        value: 'public, max-age=31536000, immutable',
+                    }
+                ]
+            },
+            {
+                source: '/:path(.+\\.webp)*',
+                headers: [
+                    {
+                        key: 'cache-control',
+                        value: 'public, max-age=31536000, immutable',
+                    }
+                ]
+            },
+            {
+                source: '/favicon/:path*',
+                headers: [
+                    {
+                        key: 'cache-control',
+                        value: 'public, max-age=31536000, immutable',
+                    }
+                ]
+            }
+        ]
+    }
 }
