4bc96e458859c4e5259bd2f1be0b95c9d070ebdb
Felix.Mushinsky <Felix.Mushinsky@perficient.com>
2021-02-12 12:25:31 +0000

    Merged PR 59: caching tweaks

--- next.config.js
+++ next.config.js
@@ -1,28 +1,30 @@
 module.exports = {
-  target: "serverless",
-  images: {
-    deviceSizes: [320, 768, 1024],
-  },
-  async headers() {
-    return [
-      {
-        source: "/:path*",
-        headers: [
-          {
-            key: "cache-control",
-            value: "public, max-age=31536000",
-          },
-        ],
-      },
-      {
-        source: "/:make([a-z-]{1,})/:model([a-z-]{1,})",
-        headers: [
-          {
-            key: "cache-control",
-            value: "public, no-cache, max-age=3600",
-          },
-        ],
-      },
-    ];
-  },
+    target: "serverless",
+    images: {
+        deviceSizes: [320, 768, 1024],
+    },
+    async headers() {
+        return [
+            {
+                source: "/",
+                headers: [
+                    {
+                        key: "cache-control",
+                        value: "public, s-maxage=31536000, max-age=3600"
+                    }
+                ]
+            },
+            {
+                source: "/:path*",
+                headers: [
+                    {
+                        key: "cache-control",
+                        value: "public, s-maxage=31536000, max-age=3600"
+                    }
+                ]
+            }
+
+
+        ];
+    },
 };
