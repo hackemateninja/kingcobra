ad0cbcd0e4cbf0220a22dd93b63e25ddd076760b
Pablo.Colaiacovo <Pablo.Colaiacovo@perficient.com>
2021-02-18 13:54:08 +0000

    Merged PR 75: Added sitemap generator
    Added sitemap generator

--- next.config.js
+++ next.config.js
@@ -1,46 +1,70 @@
-module.exports = {
-    target: "serverless",
-    images: {
-        deviceSizes: [320, 768, 1024],
-    },
-    async headers() {
-        return [
-            {
-                source: "/:path*",
-                headers: [
-                    {
-                        key: "cache-control",
-                        value: "public, s-maxage=31536000, max-age=31536000"
-                    }
-                ]
-            },
-            {
-                source: "/",
-                headers: [
-                    {
-                        key: "cache-control",
-                        value: "public, s-maxage=31536000, max-age=3600"
-                    }
-                ]
-            },
-            {
-                source: "/:make([a-z-]{1,})",
-                headers: [
-                    {
-                        key: "cache-control",
-                        value: "public, s-maxage=31536000, max-age=3600"
-                    }
-                ]
-            },
-            {
-                source: "/:make([a-z-]{1,})/:model([a-z-]{1,})",
-                headers: [
-                    {
-                        key: "cache-control",
-                        value: "public, s-maxage=31536000, max-age=3600"
-                    }
-                ]
-            }
-        ];
-    },
-};
+const withSitemap = require("next-with-sitemap");
+
+module.exports = withSitemap({
+  sitemap: {
+    baseUrl: process.env.SM_URL || "https://shop.car.com",
+    excludedPaths: [
+      "/404/",
+      "/[make]/",
+      "/[make]/[model]",
+      "/api/models/[name]",
+      "/api/models/",
+      "/api/zipcode/[id]",
+      "/api/zipcode/",
+      "/fas/",
+      "/fas/[make]/[model]/[zipcode]",
+      "/fas/[make]/[model]/",
+      "/fas/[make]/",
+      "/s2/[make]/[model]/[zipcode]",
+      "/s2/[make]/[model]/",
+      "/s2/[make]/",
+      "/s2/",
+      "/thankyou/",
+    ],
+    robots: false,
+  },
+  target: "serverless",
+  images: {
+    deviceSizes: [320, 768, 1024],
+  },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "cache-control",
+            value: "public, s-maxage=31536000, max-age=31536000",
+          },
+        ],
+      },
+      {
+        source: "/",
+        headers: [
+          {
+            key: "cache-control",
+            value: "public, s-maxage=31536000, max-age=3600",
+          },
+        ],
+      },
+      {
+        source: "/:make([a-z-]{1,})",
+        headers: [
+          {
+            key: "cache-control",
+            value: "public, s-maxage=31536000, max-age=3600",
+          },
+        ],
+      },
+      {
+        source: "/:make([a-z-]{1,})/:model([a-z-]{1,})",
+        headers: [
+          {
+            key: "cache-control",
+            value: "public, s-maxage=31536000, max-age=3600",
+          },
+        ],
+      },
+    ];
+  },
+});
