d3bd877f56c8e8c3e167780cf3a9d5f49211cf5c
Pablo Quijanoj <pablo.quijanoj@perficient.com>
2021-04-16 17:55:31 +0000

    Merged PR 129: Enabling VWO with new snippet
    Enabling VWO with the new snippet from the page
    
    Related work items: #357

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,10 @@
-import Document, { Html, Head, Main, NextScript, DocumentContext } from "next/document";
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from "next/document";
 import { ServerStyleSheet } from "styled-components";
 import { config } from "@/util/config";
 
@@ -10,7 +16,8 @@ export default class MyDocument extends Document {
     try {
       ctx.renderPage = () =>
         originalRenderPage({
-          enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
         });
 
       const initialProps = await Document.getInitialProps(ctx);
@@ -29,7 +36,7 @@ export default class MyDocument extends Document {
   }
 
   render() {
-    const loadVWO = `
+    const oldVWO = `
       window._vwo_code = window._vwo_code || (function(){
 		  var account_id=428746,
 		  settings_tolerance=2000,
@@ -39,14 +46,27 @@ export default class MyDocument extends Document {
 		  hide_element='body',
 
       /* DO NOT EDIT BELOW THIS LINE */
-      f=false,d=document,code={use_existing_jquery:function(){return use_existing_jquery;},library_tolerance:function(){return library_tolerance;},finish:function(){if(!f){f=true;var a=d.getElementById('_vis_opt_path_hides');if(a)a.parentNode.removeChild(a);}},finished:function(){return f;},load:function(a){var b=d.createElement('script');b.src=a;b.type='text/javascript';b.innerText;b.async=true;b.onerror=function(){_vwo_code.finish();};d.getElementsByTagName('head')[0].appendChild(b);},init:function(){
+      f=false,d=document,code={use_existing_jquery:function(){return use_existing_jquery;},library_tolerance:function(){return library_tolerance;},finish:function(){if(!f){f=true;var a=d.getElementById('_vis_opt_path_hides');if(a)a.parentNode.removeChild(a);}},finished:function(){return f;},load:function(a){var b=d.createElement('script');b.src=a;b.type='text/javascript';b.innerText;b.defer=true;b.async=true;b.onerror=function(){_vwo_code.finish();};d.getElementsByTagName('head')[0].appendChild(b);},init:function(){
       window.settings_timer=setTimeout('_vwo_code.finish()',settings_tolerance);var a=d.createElement('style'),b=hide_element?hide_element+'{opacity:0 !important;filter:alpha(opacity=0) !important;background:none !important;}':'',h=d.getElementsByTagName('head')[0];a.setAttribute('id','_vis_opt_path_hides');a.setAttribute('type','text/css');if(a.styleSheet)a.styleSheet.cssText=b;else a.appendChild(d.createTextNode(b));h.appendChild(a);this.load('https://dev.visualwebsiteoptimizer.com/j.php?a='+account_id+'&u='+encodeURIComponent(d.URL)+'&f='+(+is_spa)+'&r='+Math.random());return settings_timer; }};window._vwo_settings_timer = code.init(); return code; }());
+    `;
+    const newVWO = `
+      window._vwo_code = window._vwo_code || (function(){
+      var account_id=428746,
+      settings_tolerance=2000,
+      library_tolerance=2500,
+      use_existing_jquery=false,
+      is_spa=1,
+      hide_element='body',
+
+      /* DO NOT EDIT BELOW THIS LINE */
+      f=false,d=document,code={use_existing_jquery:function(){return use_existing_jquery;},library_tolerance:function(){return library_tolerance;},finish:function(){if(!f){f=true;var a=d.getElementById('_vis_opt_path_hides');if(a)a.parentNode.removeChild(a);}},finished:function(){return f;},load:function(a){var b=d.createElement('script');b.src=a;b.type='text/javascript';b.innerText;b.async=true;b.onerror=function(){_vwo_code.finish();};d.getElementsByTagName('head')[0].appendChild(b);},init:function(){
+      window.settings_timer=setTimeout(function () {_vwo_code.finish() },settings_tolerance);var a=d.createElement('style'),b=hide_element?hide_element+'{opacity:0 !important;filter:alpha(opacity=0) !important;background:none !important;}':'',h=d.getElementsByTagName('head')[0];a.setAttribute('id','_vis_opt_path_hides');a.setAttribute('type','text/css');if(a.styleSheet)a.styleSheet.cssText=b;else a.appendChild(d.createTextNode(b));h.appendChild(a);this.load('https://dev.visualwebsiteoptimizer.com/j.php?a='+account_id+'&u='+encodeURIComponent(d.URL)+'&f='+(+is_spa)+'&r='+Math.random());return settings_timer; }};window._vwo_settings_timer = code.init(); return code; }());
     `;
 
     return (
       <Html lang="en">
         <Head>
-          {/* <script dangerouslySetInnerHTML={{ __html: loadVWO }} /> */}
+          <script dangerouslySetInnerHTML={{ __html: newVWO }} />
           <script
             dangerouslySetInnerHTML={{
               __html: `
@@ -55,8 +75,16 @@ export default class MyDocument extends Document {
             }}
           ></script>
 
-          <link rel="preload" href="https://static.car.com/cars/assets/js/cookies-disabled.js" as="script" />
-          <link rel="preload" href="https://static.car.com/cars/assets/js/uts-service-uri-storage.js" as="script" />
+          <link
+            rel="preload"
+            href="https://static.car.com/cars/assets/js/cookies-disabled.js"
+            as="script"
+          />
+          <link
+            rel="preload"
+            href="https://static.car.com/cars/assets/js/uts-service-uri-storage.js"
+            as="script"
+          />
         </Head>
         <body>
           <Main />
@@ -76,8 +104,14 @@ export default class MyDocument extends Document {
               `,
             }}
           ></script>
-          <script src="https://static.car.com/cars/assets/js/cookies-disabled.js" defer></script>
-          <script src="https://static.car.com/cars/assets/js/uts-service-uri-storage.js" defer></script>
+          <script
+            src="https://static.car.com/cars/assets/js/cookies-disabled.js"
+            defer
+          ></script>
+          <script
+            src="https://static.car.com/cars/assets/js/uts-service-uri-storage.js"
+            defer
+          ></script>
           <script
             dangerouslySetInnerHTML={{
               __html: `
