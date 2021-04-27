13d423e63b11ea80920e9703b228a86ac9896252
Steven Villegas <Steven.VillegasC@perficient.com>
2021-04-27 22:28:19 +0000

    Merged PR 180: Load PWA config based on environment variable
    In this PR, I modify Next.js config to load PWA config only if `USE_PWA_CONFIG` variable is set to true in the building process.

--- next.config.js
+++ next.config.js
@@ -2,262 +2,270 @@ const withSitemap = require('next-with-sitemap');
 const { createSecureHeaders } = require('next-secure-headers');
 const withPWA = require('next-pwa');
 
-module.exports = withPWA(
-  withSitemap({
-    sitemap: {
-      baseUrl: process.env.SM_URL || 'https://shop.car.com',
-      excludedPaths: [
-        '/404/',
-        '/[make]/',
-        '/[make]/[model]',
-        '/fas/',
-        '/fas/[make]/[model]/[zipcode]',
-        '/fas/[make]/[model]/',
-        '/fas/[make]/',
-        '/s2/[make]/[model]/[zipcode]',
-        '/s2/[make]/[model]/',
-        '/s2/[make]/',
-        '/s2/',
-        '/thankyou/',
-      ],
-      robots: true,
-    },
-    images: {
-      deviceSizes: [320, 768, 1024],
-    },
-    async headers() {
-      return [
-        { source: '/(.*)', headers: createSecureHeaders() },
-        {
-          source: '/:path*',
-          headers: [
-            {
-              key: 'cache-control',
-              value: 'public, s-maxage=31536000, max-age=31536000',
-            },
-          ],
-        },
-        {
-          source: '/',
-          headers: [
-            {
-              key: 'cache-control',
-              value: 'public, s-maxage=31536000, max-age=3600',
-            },
-          ],
-        },
-        {
-          source: '/:make([a-z-]{1,})',
-          headers: [
-            {
-              key: 'cache-control',
-              value: 'public, s-maxage=31536000, max-age=3600',
-            },
-          ],
-        },
-        {
-          source: '/:make([a-z-]{1,})/:model([a-z-]{1,})',
-          headers: [
-            {
-              key: 'cache-control',
-              value: 'public, s-maxage=31536000, max-age=3600',
-            },
-          ],
-        },
-      ];
-    },
-    experimental: {
-      optimizeCss: true,
-    },
-    pwa: {
-      dest: 'public',
-      runtimeCaching: [
-        {
-          urlPattern: /^https:\/\/fonts\.(?:googleapis|gstatic)\.com\/.*/i,
-          handler: 'CacheFirst',
-          options: {
-            cacheName: 'google-fonts',
-            expiration: {
-              maxEntries: 4,
-              maxAgeSeconds: 365 * 24 * 60 * 60, // 365 days
-            },
+const enablePWAConfig = process.env.USE_PWA_CONFIG === 'true';
+
+const baseNextConfig = {
+  sitemap: {
+    baseUrl: process.env.SM_URL || 'https://shop.car.com',
+    excludedPaths: [
+      '/404/',
+      '/[make]/',
+      '/[make]/[model]',
+      '/fas/',
+      '/fas/[make]/[model]/[zipcode]',
+      '/fas/[make]/[model]/',
+      '/fas/[make]/',
+      '/s2/[make]/[model]/[zipcode]',
+      '/s2/[make]/[model]/',
+      '/s2/[make]/',
+      '/s2/',
+      '/thankyou/',
+    ],
+    robots: true,
+  },
+  images: {
+    deviceSizes: [320, 768, 1024],
+  },
+  async headers() {
+    return [
+      { source: '/(.*)', headers: createSecureHeaders() },
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'cache-control',
+            value: 'public, s-maxage=31536000, max-age=31536000',
+          },
+        ],
+      },
+      {
+        source: '/',
+        headers: [
+          {
+            key: 'cache-control',
+            value: 'public, s-maxage=31536000, max-age=3600',
+          },
+        ],
+      },
+      {
+        source: '/:make([a-z-]{1,})',
+        headers: [
+          {
+            key: 'cache-control',
+            value: 'public, s-maxage=31536000, max-age=3600',
+          },
+        ],
+      },
+      {
+        source: '/:make([a-z-]{1,})/:model([a-z-]{1,})',
+        headers: [
+          {
+            key: 'cache-control',
+            value: 'public, s-maxage=31536000, max-age=3600',
+          },
+        ],
+      },
+    ];
+  },
+  experimental: {
+    optimizeCss: true,
+  },
+};
+const pwaConfig = {
+  pwa: {
+    dest: 'public',
+    runtimeCaching: [
+      {
+        urlPattern: /^https:\/\/fonts\.(?:googleapis|gstatic)\.com\/.*/i,
+        handler: 'CacheFirst',
+        options: {
+          cacheName: 'google-fonts',
+          expiration: {
+            maxEntries: 4,
+            maxAgeSeconds: 365 * 24 * 60 * 60, // 365 days
           },
         },
-        {
-          urlPattern: /\.(?:eot|otf|ttc|ttf|woff|woff2|font.css)$/i,
-          handler: 'StaleWhileRevalidate',
-          options: {
-            cacheName: 'static-font-assets',
-            expiration: {
-              maxEntries: 4,
-              maxAgeSeconds: 7 * 24 * 60 * 60, // 7 days
-            },
+      },
+      {
+        urlPattern: /\.(?:eot|otf|ttc|ttf|woff|woff2|font.css)$/i,
+        handler: 'StaleWhileRevalidate',
+        options: {
+          cacheName: 'static-font-assets',
+          expiration: {
+            maxEntries: 4,
+            maxAgeSeconds: 7 * 24 * 60 * 60, // 7 days
           },
         },
-        {
-          urlPattern: /\.(?:jpg|jpeg|gif|png|svg|ico|webp)$/i,
-          handler: 'StaleWhileRevalidate',
-          options: {
-            cacheName: 'static-image-assets',
-            expiration: {
-              maxEntries: 64,
-              maxAgeSeconds: 24 * 60 * 60, // 24 hours
-            },
+      },
+      {
+        urlPattern: /\.(?:jpg|jpeg|gif|png|svg|ico|webp)$/i,
+        handler: 'StaleWhileRevalidate',
+        options: {
+          cacheName: 'static-image-assets',
+          expiration: {
+            maxEntries: 64,
+            maxAgeSeconds: 24 * 60 * 60, // 24 hours
           },
         },
-        {
-          urlPattern: /^\/_next\/image\?url=.+$/i,
-          handler: 'StaleWhileRevalidate',
-          options: {
-            cacheName: 'next-image',
-            expiration: {
-              maxEntries: 64,
-              maxAgeSeconds: 24 * 60 * 60, // 24 hours
-            },
+      },
+      {
+        urlPattern: /^\/_next\/image\?url=.+$/i,
+        handler: 'StaleWhileRevalidate',
+        options: {
+          cacheName: 'next-image',
+          expiration: {
+            maxEntries: 64,
+            maxAgeSeconds: 24 * 60 * 60, // 24 hours
           },
         },
-        {
-          urlPattern: /\.(?:mp3|mp4)$/i,
-          handler: 'StaleWhileRevalidate',
-          options: {
-            cacheName: 'static-media-assets',
-            expiration: {
-              maxEntries: 32,
-              maxAgeSeconds: 24 * 60 * 60, // 24 hours
-            },
+      },
+      {
+        urlPattern: /\.(?:mp3|mp4)$/i,
+        handler: 'StaleWhileRevalidate',
+        options: {
+          cacheName: 'static-media-assets',
+          expiration: {
+            maxEntries: 32,
+            maxAgeSeconds: 24 * 60 * 60, // 24 hours
           },
         },
-        {
-          urlPattern: /\.(?:js)$/i,
-          handler: 'StaleWhileRevalidate',
-          options: {
-            cacheName: 'static-js-assets',
-            expiration: {
-              maxEntries: 32,
-              maxAgeSeconds: 24 * 60 * 60, // 24 hours
-            },
+      },
+      {
+        urlPattern: /\.(?:js)$/i,
+        handler: 'StaleWhileRevalidate',
+        options: {
+          cacheName: 'static-js-assets',
+          expiration: {
+            maxEntries: 32,
+            maxAgeSeconds: 24 * 60 * 60, // 24 hours
           },
         },
-        {
-          urlPattern: /\.(?:css|less)$/i,
-          handler: 'StaleWhileRevalidate',
-          options: {
-            cacheName: 'static-style-assets',
-            expiration: {
-              maxEntries: 32,
-              maxAgeSeconds: 24 * 60 * 60, // 24 hours
-            },
+      },
+      {
+        urlPattern: /\.(?:css|less)$/i,
+        handler: 'StaleWhileRevalidate',
+        options: {
+          cacheName: 'static-style-assets',
+          expiration: {
+            maxEntries: 32,
+            maxAgeSeconds: 24 * 60 * 60, // 24 hours
           },
         },
-        {
-          urlPattern: /\.(?:json|xml|csv)$/i,
-          handler: 'NetworkFirst',
-          options: {
-            cacheName: 'static-data-assets',
-            expiration: {
-              maxEntries: 32,
-              maxAgeSeconds: 24 * 60 * 60, // 24 hours
-            },
+      },
+      {
+        urlPattern: /\.(?:json|xml|csv)$/i,
+        handler: 'NetworkFirst',
+        options: {
+          cacheName: 'static-data-assets',
+          expiration: {
+            maxEntries: 32,
+            maxAgeSeconds: 24 * 60 * 60, // 24 hours
           },
         },
-        {
-          /* Exclude /api/auth/callback/* to fix OAuth workflow in Safari without impact
-          other environment
-          Above route is default for next-auth, you may need to change it if your OAuth
-          workflow has a different callback route
-          Issue: https://github.com/shadowwalker/next-pwa/issues/131#issuecomment-821894809 */
-          urlPattern: /^\/api\/(?!auth\/callback\/).*$/i,
-          handler: 'NetworkFirst',
-          method: 'GET',
-          options: {
-            cacheName: 'apis',
-            expiration: {
-              maxEntries: 16,
-              maxAgeSeconds: 24 * 60 * 60, // 24 hours
-            },
-            networkTimeoutSeconds: 10, // fall back to cache if api does not response
-            // within 10 seconds
+      },
+      {
+        /* Exclude /api/auth/callback/* to fix OAuth workflow in Safari without impact
+        other environment
+        Above route is default for next-auth, you may need to change it if your OAuth
+        workflow has a different callback route
+        Issue: https://github.com/shadowwalker/next-pwa/issues/131#issuecomment-821894809 */
+        urlPattern: /^\/api\/(?!auth\/callback\/).*$/i,
+        handler: 'NetworkFirst',
+        method: 'GET',
+        options: {
+          cacheName: 'apis',
+          expiration: {
+            maxEntries: 16,
+            maxAgeSeconds: 24 * 60 * 60, // 24 hours
+          },
+          networkTimeoutSeconds: 10, // fall back to cache if api does not response
+          // within 10 seconds
+        },
+      },
+      {
+        urlPattern: /^\/(?!api\/).*$/i,
+        handler: 'NetworkFirst',
+        options: {
+          cacheName: 'others',
+          expiration: {
+            maxEntries: 32,
+            maxAgeSeconds: 24 * 60 * 60, // 24 hours
+          },
+          networkTimeoutSeconds: 10,
+        },
+      },
+      {
+        urlPattern: 'https://seal.digicert.com/seals/cascade/seal.min.js',
+        handler: 'StaleWhileRevalidate',
+        options: {
+          cacheName: 'digicert',
+          expiration: {
+            maxEntries: 32,
+            maxAgeSeconds: 24 * 60 * 60, // 24 hours
           },
         },
-        {
-          urlPattern: /^\/(?!api\/).*$/i,
-          handler: 'NetworkFirst',
-          options: {
-            cacheName: 'others',
-            expiration: {
-              maxEntries: 32,
-              maxAgeSeconds: 24 * 60 * 60, // 24 hours
-            },
-            networkTimeoutSeconds: 10,
+      },
+      {
+        urlPattern: 'https://cdn.oribi.io/Xy01ODMwNjU0MjM/oribi.js',
+        handler: 'StaleWhileRevalidate',
+        options: {
+          cacheName: 'oribi',
+          expiration: {
+            maxEntries: 32,
+            maxAgeSeconds: 24 * 60 * 60, // 24 hours
           },
         },
-        {
-          urlPattern: 'https://seal.digicert.com/seals/cascade/seal.min.js',
-          handler: 'StaleWhileRevalidate',
-          options: {
-            cacheName: 'digicert',
-            expiration: {
-              maxEntries: 32,
-              maxAgeSeconds: 24 * 60 * 60, // 24 hours
-            },
+      },
+      {
+        urlPattern: 'https://tags.srv.stackadapt.com/events.js',
+        handler: 'StaleWhileRevalidate',
+        options: {
+          cacheName: 'stackadapt',
+          expiration: {
+            maxEntries: 32,
+            maxAgeSeconds: 24 * 60 * 60, // 24 hours
           },
         },
-        {
-          urlPattern: 'https://cdn.oribi.io/Xy01ODMwNjU0MjM/oribi.js',
-          handler: 'StaleWhileRevalidate',
-          options: {
-            cacheName: 'oribi',
-            expiration: {
-              maxEntries: 32,
-              maxAgeSeconds: 24 * 60 * 60, // 24 hours
-            },
+      },
+      {
+        urlPattern: /http(s)?:\/\/(www\.)?google\.com\/.*/,
+        handler: 'StaleWhileRevalidate',
+        options: {
+          cacheName: 'google',
+          expiration: {
+            maxEntries: 32,
+            maxAgeSeconds: 24 * 60 * 60, // 24 hours
           },
         },
-        {
-          urlPattern: 'https://tags.srv.stackadapt.com/events.js',
-          handler: 'StaleWhileRevalidate',
-          options: {
-            cacheName: 'stackadapt',
-            expiration: {
-              maxEntries: 32,
-              maxAgeSeconds: 24 * 60 * 60, // 24 hours
-            },
+      },
+      {
+        urlPattern: /http(s)?:\/\/(www\.)?googleads.g.doubleclick\.net\/.*/,
+        handler: 'StaleWhileRevalidate',
+        options: {
+          cacheName: 'googleadd',
+          expiration: {
+            maxEntries: 32,
+            maxAgeSeconds: 24 * 60 * 60, // 24 hours
           },
         },
-        {
-          urlPattern: /http(s)?:\/\/(www\.)?google\.com\/.*/,
-          handler: 'StaleWhileRevalidate',
-          options: {
-            cacheName: 'google',
-            expiration: {
-              maxEntries: 32,
-              maxAgeSeconds: 24 * 60 * 60, // 24 hours
-            },
+      },
+      {
+        urlPattern: /http(s)?:\/\/(www\.)?sp.analytics.yahoo\.com\/.*/,
+        handler: 'StaleWhileRevalidate',
+        options: {
+          cacheName: 'yahoo',
+          expiration: {
+            maxEntries: 32,
+            maxAgeSeconds: 24 * 60 * 60, // 24 hours
           },
         },
-        {
-          urlPattern: /http(s)?:\/\/(www\.)?googleads.g.doubleclick\.net\/.*/,
-          handler: 'StaleWhileRevalidate',
-          options: {
-            cacheName: 'googleadd',
-            expiration: {
-              maxEntries: 32,
-              maxAgeSeconds: 24 * 60 * 60, // 24 hours
-            },
-          },
-        },
-        {
-          urlPattern: /http(s)?:\/\/(www\.)?sp.analytics.yahoo\.com\/.*/,
-          handler: 'StaleWhileRevalidate',
-          options: {
-            cacheName: 'yahoo',
-            expiration: {
-              maxEntries: 32,
-              maxAgeSeconds: 24 * 60 * 60, // 24 hours
-            },
-          },
-        },
-      ],
-    },
-  }),
-);
+      },
+    ],
+  },
+};
+const nextConfig = enablePWAConfig
+  ? { ...baseNextConfig, ...pwaConfig }
+  : baseNextConfig;
+const siteMapConfig = withSitemap(nextConfig);
+
+module.exports = enablePWAConfig ? withPWA(siteMapConfig) : siteMapConfig;
